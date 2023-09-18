5396ae9227067a12bfb70e387f0eab91e417ce1d
Goktug YILDIZ <goktug.7tepe@gmail.com>
2023-09-18 16:20:21 -0700

    create Shop.js file

---
+++ assets/scripts/shop.js
@@ -0,0 +1,14 @@
+import ProductList from "./productList.js";
+import ShoppingCart from "./shoppingCart.js";
+
+class Shop {
+  render() {
+    const productList = new ProductList();
+    const shoppingCart = new ShoppingCart();
+    productList.fetchProducts();
+    productList.render();
+    shoppingCart.render();
+  }
+}
+
+export default Shop;

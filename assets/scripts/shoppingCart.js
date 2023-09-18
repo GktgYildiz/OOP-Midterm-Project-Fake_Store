71362b29222219ca7e17737cdf8181012271b1a1
Goktug YILDIZ <goktug.7tepe@gmail.com>
2023-09-18 16:18:46 -0700

    create ShoppingCart.js file

---
+++ assets/scripts/shoppingCart.js
@@ -0,0 +1,10 @@
+class ShoppingCart {
+  constructor() {
+    this.items = [];
+  }
+  getTotal() {
+    //try to implement reduce method in here
+  }
+  render() {}
+}
+export default ShoppingCart;

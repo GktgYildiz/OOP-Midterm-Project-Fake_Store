510175fac1eb288d4a7ceb4d71e3c9475c2a8ad5
Goktug YILDIZ <goktug.7tepe@gmail.com>
2023-09-18 16:02:27 -0700

    create product.js file

---
+++ assets/scripts/product.js
@@ -0,0 +1,11 @@
+class Product {
+  constructor(id, title, price, description, category, image) {
+    this.id = id;
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.category = category;
+    this.image = image;
+  }
+}
+export default Product;
